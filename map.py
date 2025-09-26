b7e59172c495dcf1bc21cd85c445c7caa2ead3ca
Rashik Intisar Siddiquee <rsiddiq2@ncsu.edu>
2025-09-26 13:32:39 -0400

    Update map.py

--- map.py
+++ map.py
@@ -11,7 +11,7 @@ import os
 # ─── CONFIG ──────────────────────────────────────────────────────────────
 
 # Set your Mapbox token as environment variable
-os.environ["MAPBOX_API_KEY"] = "pk.eyJ1IjoicnNpZGRpcTIiLCJhIjoiY21jbjcwNWtkMHV5bzJpb2pnM3QxaDFtMyJ9.6T6i_QFuKQatpGaCFUvCKg"
+os.environ["MAPBOX_API_KEY"] = "pk.eyJ1IjoicnNpZGRpcTIiLCJhIjoiY21nMTRkZ21nMGptNzJrb2phOWpyMmpvZiJ9.qbQ-Pn8jaPA7ABdccrTXsg"
 
 HOURS_CSV = "fbcenc_hourly.csv"
 ODM_CSV = "ODM_CAFN_2.csv"
